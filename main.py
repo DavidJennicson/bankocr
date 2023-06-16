71080aaf357bb77db8c5f92a29f7eaf87e8420d0
DavidJennicson <85760802+DavidJennicson@users.noreply.github.com>
2023-06-16 22:24:56 +0530

    changes made

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-op=open('train.txt','r')
+import os
+
+op=open('test.txt','r')
 trainarr=[]
 for x in op.readlines():
     print(x)
     x=x.replace('\n','')
     trainarr.append(x.replace('Images/',''))
 print(trainarr)
+for x in os.listdir('Images\\'):
+    print(x)
+    extension=os.path.splitext(x)[1]
+    if x in trainarr:
+        if extension =='.jpg':
+            os.replace('Images\\' + x, 'image\\val\\' + x)
+        if extension=='.txt':
+            os.replace('Images\\' + x, 'labels\\val\\' + x)
